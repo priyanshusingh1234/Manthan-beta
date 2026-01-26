402a93c709cbcb24ac430ada2edf14b1f6984cc4
Priyanshu Singh <pkkpk2222@outlook.com>
2026-01-26 13:33:16 +0530

    Update layout.tsx

--- manthan/app/layout.tsx
+++ manthan/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
-import Header from '@/components/Header'
+
 import ClientLayout from '@/components/ClientLayout'
 
 export const metadata: Metadata = {
