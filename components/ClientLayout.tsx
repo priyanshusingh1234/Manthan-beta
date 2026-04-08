3cfe783027e8e3eff2d86449dca205e036513823
Priyanshu Kumar <pkkpk2222@outlook.com>
2026-04-08 22:47:37 +0530

    bottom nav fix

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -359,7 +359,7 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
   return (
     <>
       {!hideMainSidebar && <DesktopSidebar />}
-      <div className={`${hideMainSidebar ? 'lg:pl-0' : 'lg:pl-64'} ${showBottomNav && !hideBottomNav ? 'pb-[calc(4rem+env(safe-area-inset-bottom))]' : ''}`}>
+      <div className={`${hideMainSidebar ? 'lg:pl-0' : 'lg:pl-64'} ${showBottomNav && !hideBottomNav ? 'pb-20' : ''}`}>
         {!hideMainSidebar && !isIndividualTest && !isChatPage && <Header isMobile={isMobile} />}
         {children}
         {showBottomNav && !hideBottomNav && <BottomNav />}
