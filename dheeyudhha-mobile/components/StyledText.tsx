f29223b09645a0ea3bb843162d46b928bb1ff328
Priyanshu Kumar <pkkpk2222@outlook.com>
2026-06-01 07:54:58 +0530

    Delete dheeyudhha-mobile folder

--- dheeyudhha-mobile/components/StyledText.tsx
+++
@@ -1,6 +0,0 @@
-
-import { Text, TextProps } from './Themed';
-
-export function MonoText(props: TextProps) {
-  return <Text style={[props.style, { fontFamily: 'SpaceMono' }]} />;
-}
--- dheeyudhha-mobile/lib/version.ts
+++
@@ -1,2 +0,0 @@
-export const CURRENT_VERSION = "0.1.0";
-export const VERSION_NAME = "v0.1.0 Release Candidate";
