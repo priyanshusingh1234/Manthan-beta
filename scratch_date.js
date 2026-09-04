f8ad4d54a43443736d2b77097a33bd4f84c59a28
Priyanshu Kumar <pkkpk2222@outlook.com>
2026-09-04 07:51:37 +0530

    chore: remove untracked dev/seed scripts from github

--- scratch_date.js
+++
@@ -1,5 +0,0 @@
-const nowIST = new Date(Date.now() + 5.5 * 60 * 60 * 1000);
-const dWeek = new Date(nowIST);
-dWeek.setDate(dWeek.getDate() - dWeek.getDay());
-const currentWeekKey = `${dWeek.getFullYear()}-W${String(dWeek.getMonth() + 1).padStart(2, '0')}-${String(dWeek.getDate()).padStart(2, '0')}`;
-console.log(currentWeekKey);
