9bf7dc208f192563a707d91cb6623471191f5dc1
Priyanshu Kumar <pkkpk2222@outlook.com>
2026-09-04 07:54:51 +0530

    chore: remove all root level dev/scratch scripts

--- add_explanation_image.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE public.questions ADD COLUMN IF NOT EXISTS explanation_image_url TEXT;
--- list_models.mjs
+++
@@ -1,7 +0,0 @@
-const GOOGLE_API_KEY = process.env.GOOGLE_GENAI_API_KEY || 'AIzaSyDPJqpIRMVYwaHZkVnGcbyRLiuYS8iprSw';
-async function list() {
-  const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${GOOGLE_API_KEY}`);
-  const data = await response.json();
-  console.log(JSON.stringify(data, null, 2));
-}
-list();
